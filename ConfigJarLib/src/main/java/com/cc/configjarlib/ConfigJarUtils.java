78fa9ff3f75788e8d77a098ea8952f1ec9a37e0e
case <442415631@qq.com>
2020-08-07 10:52:14 +0700

    更换lib

---
+++ ConfigJarLib/src/main/java/com/cc/configjarlib/ConfigJarUtils.java
@@ -0,0 +1,16 @@
+package com.cc.configjarlib;
+
+/**
+ * Description:
+ *
+ * @author: CASE
+ * @date: 2020/8/7 10:47
+ */
+public class ConfigJarUtils {
+  public static String getStrFromJAR() {
+    return "这是默认版返回的JAR信息";
+    //return "这是原始版返回的JAR信息";
+    //return "这是渠道A01返回的JAR信息";
+    //return "这是渠道A02返回的JAR信息";
+  }
+}
