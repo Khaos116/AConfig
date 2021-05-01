62744aa307f87e59c240efd52b40193c1e3b402c
CASE <442415631@qq.com>
2021-05-02 00:34:45 +0700

    升级版本

--- app/src/main/java/com/cc/config/utils/AppInfoUtils.kt
+++ app/src/main/java/com/cc/config/utils/AppInfoUtils.kt
@@ -25,27 +25,27 @@ class AppInfoUtils private constructor() {
   fun getAppInfo(): String {
     val builder: StringBuilder = StringBuilder()
     builder.append("版本信息：")
-      .append("\n")
-      .append("DEBUG：")
-      .append(BuildConfig.DEBUG)
-      .append("\n")
-      .append("BUILD_TIME：")
-      .append(StringUtils.getString(R.string.build_time))
-      .append("\n")
-      .append("PACKAGE_NAME：")
-      .append(AppUtils.getAppPackageName())
-      .append("\n")
-      .append("VERSION_NAME：")
-      .append(AppUtils.getAppVersionName())
-      .append("\n")
-      .append("VERSION_CODE：")
-      .append(AppUtils.getAppVersionCode())
-      .append("\n")
-      .append("CPU：")
-      .append(getDeviceCPU())
-      .append("\n")
-      .append("SHA1：")
-      .append(AppUtils.getAppSignatureSHA1())
+        .append("\n")
+        .append("DEBUG：")
+        .append(BuildConfig.DEBUG)
+        .append("\n")
+        .append("BUILD_TIME：")
+        .append(StringUtils.getString(R.string.build_time))
+        .append("\n")
+        .append("PACKAGE_NAME：")
+        .append(AppUtils.getAppPackageName())
+        .append("\n")
+        .append("VERSION_NAME：")
+        .append(AppUtils.getAppVersionName())
+        .append("\n")
+        .append("VERSION_CODE：")
+        .append(AppUtils.getAppVersionCode())
+        .append("\n")
+        .append("CPU：")
+        .append(getDeviceCPU())
+        .append("\n")
+        .append("SHA1：")
+        .append(AppUtils.getAppSignaturesSHA1().firstOrNull() ?: "null")
     return builder.toString()
   }
 
@@ -53,9 +53,9 @@ class AppInfoUtils private constructor() {
   fun getDeviceCPU(): String {
     return try {
       BufferedReader(
-        InputStreamReader(
-          Runtime.getRuntime().exec("getprop ro.product.cpu.abi").inputStream
-        )
+          InputStreamReader(
+              Runtime.getRuntime().exec("getprop ro.product.cpu.abi").inputStream
+          )
       ).readLine()
     } catch (e: Exception) {
       ""
